42d46b9885c41046eaf92b1a04a625de7001d1bc
Carleoinserra <inserracarlo@gmail.com>
2024-11-06 14:20:14 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/pagamento.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class pagamento {
+	
+	    private String token;
+	    private Integer amount;
+		public String getToken() {
+			return token;
+		}
+		public void setToken(String token) {
+			this.token = token;
+		}
+		public Integer getAmount() {
+			return amount;
+		}
+		public void setAmount(Integer amount) {
+			this.amount = amount;
+		}
+
+	   
+	}
+
+
